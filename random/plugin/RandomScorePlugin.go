7ca4ca0fadc6c4a8a9de8605785529b71209e108
雪景 <3536159382qq.com>
2024-05-08 19:33:04 +0800

    de

--- random/plugin/RandomScorePlugin.go
+++ random/plugin/RandomScorePlugin.go
@@ -1,13 +1,19 @@
 // pkg/plugin/myplugin.go
 package plugin
 
+// import (
+// 	"context"
+// 	"log"
+// 	"math/rand" // 导入随机数生成包
+
+// 	"k8s.io/apimachinery/pkg/runtime"
+// 	"k8s.io/kubernetes/pkg/scheduler/framework"
+// )
+
 import (
 	"context"
 	"log"
-	"math/rand" // 导入随机数生成包
-
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/kubernetes/pkg/scheduler/framework"
+	"math/rand"
 )
 
 // Name is the name of the plugin used in the plugin registry and configurations.
