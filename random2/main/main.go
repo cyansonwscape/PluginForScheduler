1d47fa35ac363aff9031638895f9d9d02c8ebdd1
雪景 <3536159382qq.com>
2024-05-09 10:02:04 +0800

    123

---
+++ random2/main/main.go
@@ -0,0 +1,19 @@
+// main.go 插件调用入口
+package main
+
+import (
+	"os"
+
+	"github.com/cyansonwscape/PluginForScheduler/random2/plugin"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+)
+
+func main() {
+	command := app.NewSchedulerCommand(
+		app.WithPlugin(plugin.Name, plugin.New),
+	)
+
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
